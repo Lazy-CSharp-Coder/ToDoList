3e0c3ab5b2f34fc696ba7cc0fe9e45e7ecd86329
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-01 17:45:41 +0200

    test

---
+++ script.js
@@ -0,0 +1,5 @@
+const highPriorityCheck = document.querySelector("#highPriorityCheck");
+const mediumPriorityCheck = document.querySelector("#mediumPriorityCheck");
+const lowPriorityCheck = document.querySelector("#lowPriorityCheck");
+
+console.log(highPriorityCheck + mediumPriorityCheck + lowPriorityCheck);

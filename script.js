2237a99b08c952a2d977aceb6ad23e85b853506d
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-01 17:48:28 +0200

    test

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const mediumPriorityCheck = document.querySelector("#mediumPriorityCheck");
 const lowPriorityCheck = document.querySelector("#lowPriorityCheck");
 
 console.log(highPriorityCheck + mediumPriorityCheck + lowPriorityCheck);
+
+mediumPriorityCheck.check = true;
+
+highPriorityCheck.addEventListener("check", function() { mediumPriorityCheck.check = false; lowPriorityCheck.check = false; });
+mediumPriorityCheck.addEventListener("check", function() { highPriorityCheck.check = false; lowPriorityCheck.check = false; });
+lowPriorityCheck.addEventListener("check", function() { highPriorityCheck.check = false; mediumPriorityCheck.check = false; });

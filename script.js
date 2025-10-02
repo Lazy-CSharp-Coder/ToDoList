8ec3a93e7d190c94173f4a4a6300061ddb0e6fca
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-10-02 12:24:13 +0200

    lagt opp bedre kode

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ const highPriorityCheck = document.querySelector("#highPriorityCheck");
 const mediumPriorityCheck = document.querySelector("#mediumPriorityCheck");
 const lowPriorityCheck = document.querySelector("#lowPriorityCheck");
 
+mediumPriorityCheck.checked = true;
+
 console.log(highPriorityCheck + mediumPriorityCheck + lowPriorityCheck);
 
 highPriorityCheck.addEventListener("change", function() { mediumPriorityCheck.checked = false; lowPriorityCheck.checked = false; });
 mediumPriorityCheck.addEventListener("change", function() { highPriorityCheck.checked = false; lowPriorityCheck.checked = false; });
 lowPriorityCheck.addEventListener("change", function() { highPriorityCheck.checked = false; mediumPriorityCheck.checked = false; });
 
-mediumPriorityCheck.checked = true;
-console.log(mediumPriorityCheck.check)
+
 
 // end priority check box rutiner
 
 // dark/light mode toggle
 
 let darkMode = true;
+const darkLightIcon = document.querySelector("#darkLightIcon");
 
 function darkLightModeToggle()
 {
@@ -34,7 +36,7 @@ function darkLightModeToggle()
     }
 }
 
-const darkLightIcon = document.querySelector("#darkLightIcon");
+
 darkLightIcon.addEventListener("click", darkLightModeToggle);
 
 // end dark/light mode toggle
